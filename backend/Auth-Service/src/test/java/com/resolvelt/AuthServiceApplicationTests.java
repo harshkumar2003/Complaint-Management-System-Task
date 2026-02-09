ab1c7ef4a7234f31c7ea67b663c41f68a83a81c6
harshkumar2003 <harshk8876@gmail.com>
2026-02-09 10:27:42 +0530

    first-commit

---
+++ backend/Auth-Service/src/test/java/com/resolvelt/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resolvelt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
